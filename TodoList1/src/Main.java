85ebd1050cc358b71589abe399bc3f3c4380c002
HP <HP@LAPTOP-V02O1IRI>
2020-03-31 18:32:38 +0530

    after adding insert function

--- TodoList1/src/Main.java
+++ TodoList1/src/Main.java
@@ -1,36 +1,36 @@
 import java.util.*;
 public class Main {
 	public static void main(String[] args) {
-	int ch,n,j;
+	int choice,n,j;
 	int i=0;
-	String word;
+	String sname; //search name
 	Scanner scan=new Scanner(System.in);
-	User[] u=new User[10];
+	User[] user=new User[10];
 	while(true) {
-		System.out.println("Enter choice");
-		ch=scan.nextInt();
-		switch(ch) {
+		System.out.println("Enter choice 1.add user and todo 2.search for particular user todo 3.add more todos to a particular user list");
+		choice=scan.nextInt();
+		switch(choice) {
 		case 1:
-			u[i]=new User();
+			user[i]=new User();
 			i++;
 			break;
 		case 2:
 			System.out.println("Enter name");
-			word=scan.nextLine();
-			word=scan.nextLine();
+			sname=scan.nextLine();
+			sname=scan.nextLine();
 			for(j=0;j<i;j++) {
-				if(word.contentEquals(u[j].name)) {
-					u[j].disp();
+				if(sname.contentEquals(user[j].name)) {
+					user[j].disp();
 				}
 			}
 			break;
 		case 3:
 			System.out.println("Enter name");
-			word=scan.nextLine();
-			word=scan.nextLine();
+			sname=scan.nextLine();
+			sname=scan.nextLine();
 			for(j=0;j<i;j++) {
-				if(word.contentEquals(u[j].name)) {
-					u[j].insert();
+				if(sname.contentEquals(user[j].name)) {
+					user[j].insert();
 				}
 			}
 			break;
--- TodoList1/src/User.java
+++ TodoList1/src/User.java
@@ -4,34 +4,30 @@ public class User {
 String name;
 static int uid=0;
  Scanner scan=new Scanner(System.in);
- Todo[] todo;
+ Todo[] todo=new Todo[10];
  User(){
 	 uid++;
 	 System.out.println("Enter name");
 	 name=scan.nextLine();
 	 System.out.println("Enter number of todos");
 	 n=scan.nextInt();
-	 todo=new Todo[n];
 	 for(i=0;i<n;i++) {
 		 todo[i]=new Todo();
 	 }
  }
  void disp() {
-	 System.out.println(name);
-	 System.out.println(uid);
+	 System.out.println("Name:"+name);
+	 System.out.println("User id:"+uid);
 	 for(i=0;i<n;i++) {
-		 System.out.println(n);
 		 todo[i].display();
 	 }
  }
  void insert() {
 	 System.out.println("Enter number of todos");
-	 n=scan.nextInt();
-	 todo=new Todo[n];
-	 for(i=0;i<n;i++) {
+	 m=scan.nextInt();
+	 for(i=n;i<n+m;i++) {
 		 todo[i]=new Todo();
 	 }
 	 n=m+n;
-	 System.out.println(n);
  }
 }
